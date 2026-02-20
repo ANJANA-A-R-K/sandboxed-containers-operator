a42b8ab056ab11108a9f762175a23ebbd38c6556
Anjana A R K <anjana.a.r.k1@ibm.com>
2026-02-20 20:14:02 +0530

    test

--- controllers/addon_kernel.go
+++ controllers/addon_kernel.go
@@ -11,9 +11,10 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"    
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 const (
--- controllers/openshift_controller.go
+++ controllers/openshift_controller.go
@@ -1111,8 +1111,8 @@ func (r *KataConfigOpenShiftReconciler) processKataConfigDeleteRequest() (ctrl.R
 	}
 
 	if r.DeploymentMode == MachineConfigMode {
-		if err := r.DeleteAddonKernelMC(); err != nil { {
-			r.Log.Error(err, "failed to delete addon kernel MC")
+		if err := r.DeleteAddonKernelMC(); err != nil { 
+			r.Log.Error(err, "Failed to delete addon kernel MC")
 			return ctrl.Result{Requeue: true, RequeueAfter: 15 * time.Second}, err
 		}
 	}
@@ -1234,7 +1234,7 @@ func (r *KataConfigOpenShiftReconciler) processKataConfigInstallRequest() (ctrl.
 
 	if r.DeploymentMode == MachineConfigMode {
 		if err := r.CreateOrUpdateAddonKernelMC(machinePool); err != nil {
-			r.Log.Error(err, "failed to create/update addon kernel MachineConfig")
+			r.Log.Error(err, "Failed to create or update addon kernel MachineConfig")
 			return ctrl.Result{Requeue: true, RequeueAfter: 20 * time.Second}, err
 		}
 	}
